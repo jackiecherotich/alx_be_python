9b92d6d179505addcae965f38838dccb6693490a
jackiecherotich <40669251+jackiecherotich@users.noreply.github.com>
2025-05-22 10:51:28 +0300

    Area of a Rectangle commit changes

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,15 @@
+#rectange.py script to calculate area of a rectangle
+
+#define the variables
+
+length = 10 #  value representing the length of the rectangle.
+width =5  #value representing the width of the rectangle.
+
+#calculate the area of the rectangle
+
+area = length * width  #Formula for the area of a rectangle
+
+#Output the results 
+
+
+print ("The area of the rectangle is:" ,area,)
